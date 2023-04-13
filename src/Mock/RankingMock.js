9c4a0124b6af89956638f477eef447b0284187a6
CLHigg42 <clhigg42@gmail.com>
2023-04-13 01:01:18 -0400

    made list for rankings page, need to sort.

---
+++ src/Mock/RankingMock.js
@@ -0,0 +1,27 @@
+export const RankingMock = [
+    {
+        name: 'Elisha',
+        nickname: 'City Builder',
+        points: 6,
+    },
+    {
+        name: 'Casey',
+        nickname: 'Bob',
+        points: 8
+    },
+    {
+        name: 'Brittany',
+        nickname: 'Lilly Killer',
+        points: 3
+    },
+    {
+        name: 'Cata',
+        nickname: 'Vape Stealer',
+        points: 1
+    },
+    {
+        name: 'Angela',
+        nickname: "Bob's Daughter",
+        points: 6
+    }
+]
