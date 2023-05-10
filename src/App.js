4d49294564e9c8472371ce9722919db91164f895
Elisha <elishakimcoding@gmail.com>
2023-05-10 18:23:30 -0400

    header and navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { AdminPage } from './Pages/AdminPage';
 import { RankingsPage } from './Pages/RankingsPage';
 import { LandingPage } from './Pages/LandingPage';
 import { Header } from './Pages/Header';
+import { ErrorPage } from './Pages/ErrorPage'
+import { TournamentList } from './Pages/TournamentList';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/rankings" element={<RankingsPage />} />
         <Route path="/player" element={<PlayerPage />} />
         <Route path="/admin" element={<AdminPage/>} />
+        <Route path="/tournaments" element={<TournamentList/>} />
+        <Route path="/*" element={<ErrorPage/>} />
       </Routes>
       </div>
   );
--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,10 +1,102 @@
-import * as React from 'react';
-import { AppBar, Box, Toolbar, Typography, Button, IconButton} from '@mui/material';
-import {Menu } from '@mui/icons-material';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { 
+  AppBar, 
+  Box, 
+  Toolbar, 
+  Typography, 
+  Button, 
+  IconButton,
+  SwipeableDrawer,
+  MenuList,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  List
+} from '@mui/material';
+import {
+  Menu,
+  Security,
+  EmojiEvents,
+  Celebration,
+  AccountCircle
+ } from '@mui/icons-material';
+ import '../css/header.css'
 
 export const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  }
+
+  const openPage = (url) => {
+    navigate(url);
+    setDrawerOpen(false);
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }} >
+      <React.Fragment key='left'>
+        <SwipeableDrawer
+          style={{width: '200px'}}
+          anchor='left'
+          open={drawerOpen}
+          onClose={() => toggleDrawer()}
+          onOpen={() => toggleDrawer()}
+        >
+          <MenuList style={{width: '200%'}}>
+            <MenuItem>
+              <ListItemIcon>
+                Menu
+              </ListItemIcon>
+            </MenuItem>
+            <Divider />
+            <MenuItem
+              onClick={() => openPage('admin')}
+            >
+              <ListItemIcon>
+                <Security />
+              </ListItemIcon>
+              <ListItemText>
+                Administrator
+              </ListItemText>
+            </MenuItem>
+            <MenuItem
+              onClick={() => openPage('rankings')}
+            >
+              <ListItemIcon>
+                <EmojiEvents />
+              </ListItemIcon>
+              <ListItemText>
+                Rankings
+              </ListItemText>
+            </MenuItem>
+            <MenuItem
+              onClick={() => openPage('tournaments')}
+            >
+              <ListItemIcon>
+                <Celebration/>
+              </ListItemIcon>
+              <ListItemText>
+                Tournaments
+              </ListItemText>
+            </MenuItem>
+            <MenuItem
+              onClick={() => openPage('player')}
+            >
+              <ListItemIcon>
+                <AccountCircle/>
+              </ListItemIcon>
+              <ListItemText>
+                My Account
+              </ListItemText>
+            </MenuItem>
+          </MenuList>
+        </SwipeableDrawer>
+      </React.Fragment>
       <AppBar position="static" style={{backgroundColor: '#007F2E'}}>
         <Toolbar>
           <IconButton
@@ -13,13 +105,18 @@ export const Header = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={() => toggleDrawer()}
           >
             <Menu />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Home
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button 
+            color="inherit"
+          >
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
